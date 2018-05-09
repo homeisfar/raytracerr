7761da2a8cdd010acfa63c99112a2c2a8507ad41
Ali <home.isfar@gmail.com>
2018-05-08 22:41:55 -0400

    BASIC tracer ready. Compiles and runs, draws a unit sphere as a circle (no light source, no shadows)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,66 @@
 extern crate image;
 
-use image::DynamicImage;
+use std::fs::File;
+use image::{DynamicImage, GenericImage, Rgba};
 
 mod scene_structs;
-use scene_structs::Scene;
+use scene_structs::*;
 
 fn main() {
     println!("Hello, world!");
+
+    // scene: Scene = {
+    //     width: 640,
+    //     height: 480,
+    //     fov: 90.0,
+    //     sphere: 
+    // }
+    // render()
+
+    let img = render(&create_simple_scene());
+
+    // let ref mut fout = File::create("output.png").unwrap();
+    // img.save(fout).unwrap();
+    img.save("c:\\Users\\Alih\\Desktop\\Output.png").unwrap();
+}
+
+fn create_simple_scene() -> Scene {
+    let scene = Scene {
+        width: 800,
+        height: 200,
+        fov: 90.0,
+        sphere: Sphere {
+            center: Point {
+                x: 0.0,
+                y: 0.0,
+                z: -5.0,
+            },
+            radius: 1.0,
+            color: Color {
+                r: 0.4,
+                g: 1.0,
+                b: 0.4,
+            },
+        },
+    };
+    scene
 }
 
 pub fn render(scene: &Scene) -> DynamicImage {
-    DynamicImage::new_rgb8(scene.width, scene.height)
+    let mut image = DynamicImage::new_rgb8(scene.width, scene.height);
+    let black = Rgba([0,0,0,255]);
+    for x in 0..scene.width {
+        for y in 0..scene.height {
+            let ray = Ray::create_prime_ray(x, y, scene);
+
+            if scene.sphere.intersects(&ray) {
+                image.put_pixel(x, y, Rgba([255,0,0,255]));
+            } else {
+                image.put_pixel(x, y, black);
+            }
+        }
+    }
+    image
 }
 
 #[test]
@@ -27,9 +77,9 @@ fn test_scene_render() {
             },
             radius: 1.0,
             color: Color {
-                red: 0.4,
-                green: 1.0,
-                blue: 0.4,
+                r: 0.4,
+                g: 1.0,
+                b: 0.4,
             },
         },
     };
--- src/scene_structs.rs
+++ src/scene_structs.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub};
+
 pub struct Color {
     pub r: f32,
     pub g: f32,
@@ -5,9 +7,9 @@ pub struct Color {
 }
 
 pub struct Point {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Point {
@@ -16,6 +18,18 @@ impl Point {
             x: 0.0,
             y: 0.0,
             z: 0.0,
+        }
+    }
+}
+
+impl<'a, 'b> Sub<&'b Point> for &'a Point {
+    type Output = Vector3;
+
+    fn sub(self, other: &'b Point) -> Vector3 {
+        Vector3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
         }
     }
 }
@@ -50,6 +64,9 @@ impl Vector3 {
             z: self.z * inv_len,
         }
     }
+    pub fn dot(&self, other: &Vector3) -> f64 {
+        (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+    }
 }
 
 pub struct Sphere {
@@ -76,8 +93,11 @@ pub struct Ray {
 
 impl Ray {
     pub fn create_prime_ray(x: u32, y: u32, scene: &Scene) -> Ray {
-        let camera_sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
-        let camera_sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+        let fov_adjustment = (scene.fov.to_radians() / 2.0).tan();
+        // Assume width > height
+        let aspect_ratio = (scene.width as f64) / (scene.height as f64);
+        let camera_sensor_x = ((((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0) * aspect_ratio) * fov_adjustment;
+        let camera_sensor_y = (1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0) * fov_adjustment;
 
         Ray {
             origin: Point::zero(),
@@ -87,5 +107,32 @@ impl Ray {
                 z: -1.0,
             }.normalize_to_frame(),
         }
+    }
+}
+
+
+/***************************/
+/* Scene object properties */
+/***************************/
+
+pub trait Intersectable {
+    fn intersects(&self, ray: &Ray) -> bool;
+}
+
+impl Intersectable for Sphere {
+    fn intersects(&self, ray: &Ray) -> bool {
+        // let length = Vector3 {
+        //     x: self.center.x - ray.origin.x,
+        //     y: self.center.y - ray.origin.y,
+        //     z: self.center.z - ray.origin.z,
+        // };
+        let length: Vector3 = &self.center - &ray.origin;
+
+        let adjacent2 = length.dot(&ray.direction);
+
+        let d2 = length.dot(&length) - (adjacent2.powf(2.0));
+
+        d2 < (self.radius.powf(2.0))
+        // false
     }
 }
